0810161de897212b8d61510d3b448bc3e7232cb1
Armando <dam.armandomontejano@gmail.com>
2020-09-27 15:53:13 +0200

    ajustes

--- js/js.js
+++ js/js.js
@@ -1,5 +1,4 @@
 import getHTML from "./getHTML.js";
-import getResult from "./getResult.js";
 
 let array = [];
 let buttonsArray = [];
@@ -47,7 +46,36 @@ let counter = setInterval(timer, 1000);
                 
                 } else {
                     document.querySelector(".card").remove();
-                    getResult($main, questionsRight);
+                    getHTML({
+                        url:"result_trivia.html",
+                        success: (html) => {
+                        $main.innerHTML = html
+                        document.querySelector(".card-body").innerHTML = `<h5 class="card-title">Haciendo recuento...</h5><img src='tail-spin.svg' />`
+                    
+                        setTimeout(() => {
+                          document.querySelector(".card-body").innerHTML = `<h5 class="card-title recuento">Has acertado: ${questionsRight} respuestas.</h5><div class="rating">
+                    
+                          <input type="radio" name="rating" id="r1">
+                          <label for="r1"></label>
+                    
+                          <input type="radio" name="rating" id="r2">
+                          <label for="r2"></label>
+                    
+                          <input type="radio" name="rating" id="r3">
+                          <label for="r3"></label>
+                    
+                          <input type="radio" name="rating" id="r4">
+                          <label for="r4"></label>
+                    
+                          <input type="radio" name="rating" id="r5">
+                          <label for="r5"></label>
+                    
+                          </div>`;
+                        }, 3000);
+                      },
+                    
+                        error: (err) => $main.innerHTML = `<h1>${err}</h1>`
+                    });
                 }
             } 
         }
@@ -191,7 +219,36 @@ d.addEventListener("click", e => {
             document.querySelector(".card").remove();
                 gameIsEnd = true;
                 
-                getResult($main, questionsRight);
+                getHTML({
+                    url:"result_trivia.html",
+                    success: (html) => {
+                    $main.innerHTML = html
+                    document.querySelector(".card-body").innerHTML = `<h5 class="card-title">Haciendo recuento...</h5><img src='tail-spin.svg' />`
+                
+                    setTimeout(() => {
+                      document.querySelector(".card-body").innerHTML = `<h5 class="card-title recuento">Has acertado: ${questionsRight} respuestas.</h5><div class="rating">
+                
+                      <input type="radio" name="rating" id="r1">
+                      <label for="r1"></label>
+                
+                      <input type="radio" name="rating" id="r2">
+                      <label for="r2"></label>
+                
+                      <input type="radio" name="rating" id="r3">
+                      <label for="r3"></label>
+                
+                      <input type="radio" name="rating" id="r4">
+                      <label for="r4"></label>
+                
+                      <input type="radio" name="rating" id="r5">
+                      <label for="r5"></label>
+                
+                      </div>`;
+                    }, 3000);
+                  },
+                
+                    error: (err) => $main.innerHTML = `<h1>${err}</h1>`
+                });
             } else {
                 nextQuestion();
             }
