b070f65a39c0d8cae03ad6d87f49dcbc87163ae2
Armando <dam.armandomontejano@gmail.com>
2020-10-03 00:03:28 +0200

    Restyling y añado resumen de partida

--- js/getResult.js
+++ js/getResult.js
@@ -1,55 +1,85 @@
 import getHTML from "./getHTML.js"
-let d = document;
-export default function geResult($main, questionsRight, nQuestions){
+export default function geResult($main, questionsRight, nQuestions, array, myAnwers){
     
     getHTML({
         url:"result_trivia.html",
         success: (html) => {
-        $main.innerHTML = html
-        document.querySelector(".card-body").innerHTML = `<h5 class="card-title">Haciendo recuento...</h5><img src='tail-spin.svg' />`
+        $main.innerHTML = html;
+        document.querySelector(".card-body").innerHTML = `<h5 class="card-title">Haciendo recuento...</h5><img src='tail-spin.svg' />`;
     
         setTimeout(() => {
-          document.querySelector(".card-body").innerHTML = `<h5 class="card-title recuento">Has acertado: ${questionsRight} respuestas.</h5><div class="rating">
-    
-          <input type="radio" name="rating" id="r1" disabled>
-          <label for="r1"></label>
-    
-          <input type="radio" name="rating" id="r2" disabled>
-          <label for="r2"></label>
-    
-          <input type="radio" name="rating" id="r3" disabled>
-          <label for="r3"></label>
-    
-          <input type="radio" name="rating" id="r4" disabled>
-          <label for="r4"></label>
-    
-          <input type="radio" name="rating" id="r5" disabled>
-          <label for="r5"></label>
-    
-          </div>`;
+          document.querySelector(".card-body").innerHTML = `<h5 class="card-title recuento">Has acertado: ${questionsRight} respuestas.</h5>
+                                                              <div class="rating">
+                                                                <input type="radio" name="rating" id="r1" disabled>
+                                                                <label for="r1"></label>
+                                                          
+                                                                <input type="radio" name="rating" id="r2" disabled>
+                                                                <label for="r2"></label>
+                                                          
+                                                                <input type="radio" name="rating" id="r3" disabled>
+                                                                <label for="r3"></label>
+                                                          
+                                                                <input type="radio" name="rating" id="r4" disabled>
+                                                                <label for="r4"></label>
+                                                          
+                                                                <input type="radio" name="rating" id="r5" disabled>
+                                                                <label for="r5"></label>
+                                                              </div>`;
           setTimeout(() => {
-            let percent = (questionsRight/nQuestions)*100;
-                                          
-          let intpercent = Math.round(percent);
-          
-          if (intpercent === 0) {
-            return
-          }
-          else if (intpercent <= 20) {
-            d.getElementById("r5").checked = true;
-          }
-          else if (intpercent > 20 && intpercent <= 40) {
-            d.getElementById("r4").checked = true;
-          } 
-          else if (intpercent > 40 && intpercent <= 60) {
-            d.getElementById("r3").checked = true;
-          }
-          else if (intpercent > 60 && intpercent <=80) {
-            d.getElementById("r2").checked = true;
-          }
-          else {
-            d.getElementById("r1").checked = true;
-          }
+            let percent = (questionsRight/nQuestions)*100;                     
+            let intpercent = Math.round(percent);
+            
+            document.querySelector(".totalQuestions").textContent = `${nQuestions}`;
+            document.querySelector(".score").textContent = `${intpercent}%`;
+            document.querySelector(".correctAnswers").textContent = `${questionsRight}/${nQuestions}`;
+
+
+            const $template = document.getElementById("card-template").content;
+            const $fragment = document.createDocumentFragment();
+
+            array.forEach((el, i) => {
+              
+              el.correct_answer === myAnwers[i]
+              ? $template.querySelector(".resume").classList.add("right")
+              : $template.querySelector(".resume").classList.add("wrong");
+
+              $template.querySelector(".category").innerHTML = `Category: <strong>${el.category}</strong>`;
+              $template.querySelector(".difficulty").innerHTML = `Difficulty: <strong>${el.difficulty}</strong>`;
+              $template.querySelector(".question").innerHTML = `Question: <strong>${el.question}</strong>`;
+              $template.querySelector(".correctAnswer").innerHTML = `Correct answer: <strong>${el.correct_answer}</strong>`;
+              $template.querySelector(".myAnswer").innerHTML = `Your answer: <strong>${myAnwers[i] || "-"}</strong>`;
+
+              let $clone = document.importNode($template, true);
+              $fragment.appendChild($clone);
+
+              debugger
+              $template.querySelector(".resume").classList.remove("right")
+              $template.querySelector(".resume").classList.remove("wrong");
+
+            });
+           
+            document.querySelector(".resumes").appendChild($fragment);
+
+            if (intpercent === 0) {
+              return
+            }
+            else if (intpercent <= 20) {
+              document.getElementById("r5").checked = true;
+            }
+            else if (intpercent > 20 && intpercent <= 40) {
+              document.getElementById("r4").checked = true;
+            } 
+            else if (intpercent > 40 && intpercent <= 60) {
+              document.getElementById("r3").checked = true;
+            }
+            else if (intpercent > 60 && intpercent <=80) {
+              document.getElementById("r2").checked = true;
+            }
+            else {
+              document.getElementById("r1").checked = true;
+            }
+
+            
           }, 500);
         }, 3000);
       },
--- js/js.js
+++ js/js.js
@@ -3,32 +3,34 @@ import getResult from "./getResult.js";
 
 let array = [];
 let buttonsArray = [];
-const d = document;
-let $form = d.querySelector("form");
-let $selectCategory = d.getElementById("selectCategory");
-let $nQuestions = d.getElementById("nQuestions");
+let myAnswers = [];
+let categoryId;
+let selectCategory;
+let nQuestions;
+let diffculty = "";
 let url;
-let $main = d.querySelector("main");
+let $main = document.querySelector("main");
 let questionsRight = 0;
 let currentQuestion = 0;
 let isAnswer = false;
 let gameIsEnd = false;
-let $progressBar
+let $progressBar;
 
 const gameStart = () => {
 let counter = setInterval(timer, 1000); 
 
     function timer() {
-        if( !gameIsEnd) {
+        if(!gameIsEnd) {
             
-            $progressBar = d.getElementById("progress-bar");
+            $progressBar = document.getElementById("progress-bar");
             console.log($progressBar.value--);
         }
 
         if($progressBar.value <= 0) {
+            myAnswers.push("");
 
             if(!isAnswer) {
-                if (currentQuestion <= parseInt($nQuestions.value)-1) {
+                if (currentQuestion <= nQuestions) {
                     const Toast = Swal.mixin({
                         toast: true,
                         position: 'center',
@@ -46,7 +48,7 @@ let counter = setInterval(timer, 1000);
                 
                 } else {
                     document.querySelector(".card").remove();
-                    getResult($main, questionsRight, parseInt($nQuestions.value));
+                    getResult($main, questionsRight, nQuestions, array, myAnswers);
                 }
             } 
         }
@@ -65,17 +67,20 @@ let counter = setInterval(timer, 1000);
 const nextQuestion = () => {
     isAnswer = false;
     
-    let card = `<div class="card col-12"> 
-                    <span class="badge badge-pill badge-primary">${++currentQuestion}/${$nQuestions.value}</span>
-                    <progress id="progress-bar" value="10" max="10"></progress>
+    let card = `<div class="card col-12 mb-5"> 
+                    <span class="badge badge-pill badge-primary">${++currentQuestion}/${nQuestions}</span>
+                    <progress id="progress-bar" value="15" max="15"></progress>
                     <div class="card-body">
-                    <h5 class="card-title">${array[currentQuestion-1].category}</h5>
+                    <div class="row">
+                    <h6 class="card-title col-12 p-0">${array[currentQuestion-1].category}</h6>
+                    <h6 class="card-title col-12 p-0">Diffculty: ${array[currentQuestion-1].difficulty}</h6>
+                    </div> 
                     <p class="card-text">${array[currentQuestion-1].question}</p>
                     <div class="row buttons d-block">
                     </div>
                     </div>
                 </div>
-                <a href="index.html" class="btn btn-primary col-12" type="submit">Ir a inicio</a>`;
+                <a href="index.html" class="btn btn-primary col-12" type="submit">Go to home</a>`;
 
     $main.innerHTML = card
                     
@@ -84,73 +89,23 @@ const nextQuestion = () => {
     }
 }
           
-d.addEventListener("DOMContentLoaded", e => {
+document.addEventListener("DOMContentLoaded", e => {
     fetch("https://opentdb.com/api_category.php")
     .then(res => res.json())
     .then(res => {
-        res.trivia_categories.forEach(e => {
-            const $option = d.createElement("option");
-            $option.value = e.id; 
-            $option.textContent = e.name;
-
-            $selectCategory.appendChild($option);
-        });
+        let divsCat = document.querySelectorAll(".category");
+        
+        for (let i = 0; i < divsCat.length-1; i++) {
+            divsCat[i+1].dataset.id = res.trivia_categories[i].id;
+        };
     })
 })
-
-d.addEventListener("submit", e => {      
-    e.preventDefault();
-    $form.classList.add("none");
-
-    if($selectCategory.value === "any") {
-        url = `https://opentdb.com/api.php?amount=${$nQuestions.value}`;
-    } else {
-        url = `https://opentdb.com/api.php?amount=${$nQuestions.value}&category=${$selectCategory.value}`;
-    }
-
-    fetch(url)
-    .then(res => res.json())
-    .then(res => {
-        array = res.results;
-        
-        res.results.forEach(e => {
-        let answers = [];
-        answers.push(e.correct_answer);
-        answers.push(...e.incorrect_answers);
-
-        const answerSort = (a, b) => {
-            return 0.5 - Math.random();
-        } 
-
-        let answerAleatory = answers.sort(answerSort);
-        let buttons = [];
-        
-        for (let i = 0; i < answers.length; i++) {
-            const button = document.createElement("button");
-            button.classList.add("btn", "btn-primary", "btn-question","col-12")
-            button.textContent = answerAleatory[i]
-            if(answerAleatory[i] === e.correct_answer) {
-            button.dataset.question = "true";
-            } else {
-            button.dataset.question = "false";
-            }
-            buttons.push(button)
-        }
-        buttonsArray.push(buttons);
-        });
-
-        gameStart();
-
-        nextQuestion();
-    });
-});
-          
-d.addEventListener("click", e => {
+  
+document.addEventListener("click", e => {
 
     if(e.target.matches(".btn-question") || e.target.matches(".btn-question *")) {
-
         isAnswer = true;
-        
+        myAnswers.push(e.target.textContent)
         document.querySelectorAll(".btn-question").forEach(e => {
             e.disabled = true;
         })
@@ -186,18 +141,116 @@ d.addEventListener("click", e => {
         }
 
         setTimeout(() => {
-            if(currentQuestion === parseInt($nQuestions.value)) {
+            if(currentQuestion === nQuestions) {
             document.querySelector(".card").remove();
                 gameIsEnd = true;
                 
-                getResult($main, questionsRight, parseInt($nQuestions.value));
+                getResult($main, questionsRight, nQuestions, array, myAnswers);
             } else {
                 nextQuestion();
             }
         }, 1000);
     }
-})
 
-$form.addEventListener("keyup", e => {
-    (parseInt($nQuestions.value) < 1 || $nQuestions.value === "")  ? d.getElementById("startGame").disabled = true : d.getElementById("startGame").disabled = false;
+    if(e.target.matches(".category") || e.target.matches(".category *")) {
+        categoryId = e.target.dataset.id || e.target.closest(".category").dataset.id;      
+        selectCategory = e.target.innerText || e.target.nextElementSibling.innerText;
+        
+        document.querySelector("body").insertAdjacentHTML("afterbegin", `<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLongTitle">${selectCategory}</h5>
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                <div class="flex flex-column">
+                                                                    <p><p>Select total number of a questions.</p></p>
+                                                                    <form class="d-flex p-0">
+                                                                    <label><input class="nQuestions" type="radio" name="nQ" value=10 checked><span>10</span></label>
+                                                                    <label><input class="nQuestions" type="radio" name="nQ" value=20><span>20</span></label>
+                                                                    <label><input class="nQuestions" type="radio" name="nQ" value=30><span>30</span></label>
+                                                                    <label><input class="nQuestions" type="radio" name="nQ" value=40><span>40</span></label>
+                                                                    <label><input class="nQuestions" type="radio" name="nQ" value=50><span>50</span></label>
+                                                                    </form>
+                                                                </div>
+                                                                
+                                                                <div class="flex flex-column">
+                                                                    <p><p>Select diffculty.</p></p>
+                                                                    <form class="d-flex p-0">
+                                                                    <label><input class="diffculty" type="radio" name="diffculty" value="any" checked><span>Any</span></label>
+                                                                    <label><input class="diffculty" type="radio" name="diffculty" value="easy"><span>Easy</span></label>
+                                                                    <label><input class="diffculty" type="radio" name="diffculty" value="medium"><span>Medium</span></label>
+                                                                    <label><input class="diffculty" type="radio" name="diffculty" value="hard"><span>Hard</span></label>
+                                                                    </form>
+                                                                </div>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                <button id="startGame" type="button" class="btn btn-primary">Start game!</button>
+                                                                </div>
+                                                            </div>
+                                                            </div>
+                                                        </div>`);
+        $('#exampleModalCenter').modal('show');
+    }
+
+    if(e.target.matches(".nQuestions") || e.target.matches(".category *")) {
+        nQuestions = e.target.value;
+    }
+
+    if(e.target.matches(".diffculty") || e.target.matches(".category *")) {
+        diffculty = e.target.value;
+    }
+
+    if(e.target.matches("#startGame")) {
+        if(!categoryId && !diffculty) {
+            if(!nQuestions) nQuestions = 10;
+            url = `https://opentdb.com/api.php?amount=${nQuestions}`;
+        } else if (!diffculty) {
+            if(!nQuestions) nQuestions = 10;
+            url = `https://opentdb.com/api.php?amount=${nQuestions}&category=${categoryId}`;
+        } else {
+            if(!nQuestions) nQuestions = 10;
+            url = `https://opentdb.com/api.php?amount=${nQuestions}&category=${categoryId}&difficulty=${diffculty}`;
+        }
+        
+        $('#exampleModalCenter').modal('hide');
+
+        fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            array = res.results;
+            
+            res.results.forEach(e => {
+            let answers = [];
+            answers.push(e.correct_answer);
+            answers.push(...e.incorrect_answers);
+
+            const answerSort = (a, b) => {
+                return 0.5 - Math.random();
+            } 
+
+            let answerAleatory = answers.sort(answerSort);
+            let buttons = [];
+            
+            for (let i = 0; i < answers.length; i++) {
+                const button = document.createElement("button");
+                button.classList.add("btn", "btn-primary", "btn-question","col-12")
+                button.textContent = answerAleatory[i]
+                if(answerAleatory[i] === e.correct_answer) {
+                button.dataset.question = "true";
+                } else {
+                button.dataset.question = "false";
+                }
+                buttons.push(button)
+            }
+            buttonsArray.push(buttons);
+            });
+            
+            gameStart();
+            nextQuestion();
+        });
+    }
 })
